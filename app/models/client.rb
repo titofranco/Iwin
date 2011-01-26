c14fefda81316ce00bd04c76a1aef33da72daad5
titofranco <titofranco@gmail.com>
2011-01-26 12:00:33 -0500

    Add mongo attributes to the model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,11 @@
-class Client < ActiveRecord::Base
+class Client
+
+include MongoMapper::Document
+
+key :identificacion , String, :required => true
+key :nombre_completo , String, :required => true
+key :edad , Integer
+key :ciudad, String
+key :sexo , String
+
 end
